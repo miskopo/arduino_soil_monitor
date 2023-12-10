5855df2073a3931110fbe3ded8425d23285031d9
Michal Polovka <michal.polovka@gmail.com>
2023-12-10 15:47:36 +0100

    Add control buttons
    Left button - soil monitor
    Middle button - toggle display backlight
    Right button - air temperature and humidity
    
    Signed-off-by: Michal Polovka <michal.polovka@gmail.com>

--- soilMonitor/soilMonitor.ino
+++ soilMonitor/soilMonitor.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 
 #include "Adafruit_SHT4x.h"
-#include <LiquidCrystal.h>// include the library code
+#include <LiquidCrystal.h>
+#include <ezButton.h>
 
 Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 LiquidCrystal lcd(9, 10, 4, 5, 6, 7);// Creates an LC object. Parameters: (rs, enable, d4, d5, d6, d7) 
@@ -12,6 +13,19 @@ const int noSensors = 4;
 const int sensorMax = 1023;
 const int sensorMin = 400;
 
+const int leftButtonPin = 11;
+const int midButtonPin = 12;
+const int rightButtonPin = 13;
+
+
+ezButton leftButton = ezButton(leftButtonPin);
+ezButton midButton = ezButton(midButtonPin);
+ezButton rightButton = ezButton(rightButtonPin);
+
+// lcd status
+bool lcdOn = true;
+int backlightPin = 8;
+
 int sensorVal[] = {1023, 1023, 1023, 1023};
 
 void setup() {
@@ -21,14 +35,45 @@ void setup() {
   sht4.begin();
   sht4.setPrecision(SHT4X_HIGH_PRECISION); // highest resolution
   sht4.setHeater(SHT4X_NO_HEATER); // no heater
+
+  leftButton.setDebounceTime(50);
+  midButton.setDebounceTime(50);
+  rightButton.setDebounceTime(50);
+
+  pinMode(backlightPin, OUTPUT);
+
 }
 
 void loop() { 
-  // temperature_humidity();
-  sensorTable();
+  leftButton.loop();
+  midButton.loop();
+  rightButton.loop();
+
+  if (leftButton.isPressed()) {
+    
+    sensorTable();
+  }
+
+  if (midButton.isPressed()) {
+    if (lcdOn){
+      lcd.noDisplay();
+      digitalWrite(backlightPin, LOW);
+      lcdOn = false;
+    } else {
+      lcd.display();
+      digitalWrite(backlightPin, HIGH);
+      lcdOn = true;
+    }
+  }
+
+  if (rightButton.isPressed()) {
+    temperatureHumidity();
+  }
+
 }
 
 void sensorTable(){
+  lcd.clear();
 
   for (int i = 0; i < noSensors; ++i){
     sensorVal[i] = analogRead(sensors[i]);
@@ -38,9 +83,10 @@ void sensorTable(){
   }
 
   // print the vaules
+  lcd.setCursor(0,0);
   lcd.print("1: "); lcd.print(sensorVal[0]); lcd.print("%");
   lcd.print("2: "); lcd.print(sensorVal[1]); lcd.print("%");
-
+  lcd.setCursor(0,1);
   lcd.print("3: "); lcd.print(sensorVal[2]); lcd.print("%");
   lcd.print("4: "); lcd.print(sensorVal[3]); lcd.print("%");
 
@@ -59,14 +105,11 @@ void sensorTable(){
     lcd.print(sensorVal[i]);
     lcd.print("%  ");
   }
-
-
-
-  delay(5000);
-  lcd.clear();
 }
 
-void temperature_humidity() {
+void temperatureHumidity() {
+  lcd.clear();
+
   sensors_event_t humidity, temp; // temperature and humidity variables
  
   sht4.getEvent(&humidity, &temp);
@@ -76,7 +119,4 @@ void temperature_humidity() {
 
   lcd.setCursor(0,1);
   lcd.print("Humidity: "); lcd.print(humidity.relative_humidity); lcd.print("%");
- 
-  delay(5000);
-  lcd.clear();
 }
